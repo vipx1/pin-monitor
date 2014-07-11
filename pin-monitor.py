29f29e7de47fedd1fa96a6a2741b39d312f46a42
Stephen Smith <stephen@vipx1.com>
2014-07-11 14:09:27 +0100

    Changed some names of objects to make code more readable and added somme more comments.

--- pin-monitor.py
+++ pin-monitor.py
@@ -13,11 +13,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 LED_01 = 4
 LED_02 = 23
-SWITCH_01 = 24
+ALARM_CONTACT_01 = 24
 
 # Pull up resistor, switch against ground!
-GPIO.setup(SWITCH_01, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# Set up outputs
+GPIO.setup(ALARM_CONTACT_01, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Set up outputs and reset any LED's that might be on
 GPIO.setup(LED_01, GPIO.OUT)
 GPIO.setup(LED_02, GPIO.OUT)
 GPIO.output(LED_01, GPIO.LOW)
@@ -25,8 +25,8 @@ GPIO.output(LED_02, GPIO.LOW)
 
 # </editor-fold>
 
-# Set valid time back one minute so first alarm can be activated immediately after service start
-valid_alarm_time_stamp = datetime.datetime.now() - timedelta(0, 60)
+# Set valid time back one minute so first Alarm can be activated immediately after service start
+alarm_time_stamp = datetime.datetime.now() - timedelta(0, 60)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-x', '--exit', help="exit pin-monitor service", action="store_true")
@@ -48,23 +48,29 @@ try:
     while 1:
         if args.exit:
             clean_exit()
-        if GPIO.input(SWITCH_01) == 0 and \
-                        (datetime.datetime.now() - valid_alarm_time_stamp).seconds > timedelta(0, 60).seconds:
+        # If alarm contact is low and the time difference between now and last alarm is greater than 60 seconds..
+        if GPIO.input(ALARM_CONTACT_01) == 0 and \
+                        (datetime.datetime.now() - alarm_time_stamp).seconds > timedelta(0, 60).seconds:
             # Switch off flashing LED
             if GPIO.input(LED_01) == 1:
                 GPIO.output(LED_01, GPIO.LOW)
-
-            valid_alarm_time_stamp = datetime.datetime.now()
+            # reset alarm activation time stamp
+            alarm_time_stamp = datetime.datetime.now()
+            # Switch on second LED to show Pi is busy sending email
             GPIO.output(LED_02, GPIO.HIGH)
+            # send an email
             ec.send_email_image_conf(ec)
+            # Switch off second LED to show Pi is finished sending email
             GPIO.output(LED_02, GPIO.LOW)
+            # have a rest :)
             sleep(1)
         # Flash LED fast while no new Alarm is possible
-        while (datetime.datetime.now() - valid_alarm_time_stamp).seconds < timedelta(0, 60).seconds:
+        while (datetime.datetime.now() - alarm_time_stamp).seconds < timedelta(0, 60).seconds:
             current_value = GPIO.input(LED_01)
             GPIO.output(LED_01, not current_value)
             sleep(0.15)
 
+        # Normal operation, waiting for new Alarm
         current_value = GPIO.input(LED_01)
         GPIO.output(LED_01, not current_value)
         sleep(0.5)
